ec70e4354b89b7e10a205ea87bf3269cc3051eb1
talal-amjad0 <talal.amjad@codingcops.com>
2024-11-07 19:50:15 +0500

    Added pages via controller generator

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -25,7 +25,7 @@ class BlogsController < ApplicationController
 
     respond_to do |format|
       if @blog.save
-        format.html { redirect_to @blog, notice: "Blog was successfully created." }
+        format.html { redirect_to blogs_path, notice: "Blog was successfully created." }
       else
         format.html { render :new, status: :unprocessable_entity }
       end
