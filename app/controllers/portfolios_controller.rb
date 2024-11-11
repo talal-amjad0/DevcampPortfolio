c8af0ff09e1ea927e928065b7b3e43ed61ea41e0
talal-amjad0 <talal.amjad@codingcops.com>
2024-11-11 14:08:30 +0500

    Added Destroy functionality

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -12,6 +12,10 @@ class PortfoliosController < ApplicationController
         @portfolio_item = Portfolio.find(params[:id])
     end
 
+    def show
+        @portfolio_item = Portfolio.find(params[:id])
+    end
+
     def create
         @portfolio_item = Portfolio.new( params.require(:portfolio).permit(:title, :subtitle,:body))
 
@@ -32,6 +36,16 @@ class PortfoliosController < ApplicationController
             else
                 format.html { render :edit, status: :unprocessable_entity }
             end
+        end
+
+    end
+
+    def destroy
+        @portfolio_item = Portfolio.find(params[:id])
+        @portfolio_item.destroy
+
+        respond_to do |format|
+            format.html {redirect_to portfolios_path}
         end
 
     end
