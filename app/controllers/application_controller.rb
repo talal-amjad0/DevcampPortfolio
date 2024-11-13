2e62686630dc2e63a3e1df8e01c56b491e479d68
talal-amjad0 <talal.amjad@codingcops.com>
2024-11-13 13:10:17 +0500

    Implementing Data sharing throung Session

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,6 @@ class ApplicationController < ActionController::Base
   allow_browser versions: :modern
 
   include DeviseWhitelist
+  include SetSource
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  
+  validates_presence_of :name
+  #Method to extract the first name from name 
+  def first_name
+    self.name.split.first
+  end
+
+  #Method to extract the last  name from name
+  def last_name
+    self.name.split.last
+  end
 end
