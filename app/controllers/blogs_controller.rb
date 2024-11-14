8a0b5716fd2ae92e96ababb8caea486db274412d
talal-amjad0 <talal.amjad@codingcops.com>
2024-11-14 13:45:15 +0500

    Implemented Role based access using peterGate

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,5 +1,6 @@
 class BlogsController < ApplicationController
   before_action :set_blog, only: %i[ show edit update destroy toggle_status ]
+  access all: [:show, :index], user: {except: [:destroy, :new, :create, :update, :edit]}, site_admin: :all
 
   # GET /blogs or /blogs.json
   def index
--- app/controllers/concerns/current_user_concern.rb
+++ app/controllers/concerns/current_user_concern.rb
@@ -3,7 +3,16 @@ module CurrentUserConcern
 
     require 'ostruct'
     def current_user
-        super || OpenStruct.new(name:"Guest User", first_name:"Guest User", last_name:"Guest User", email:"guest@gmail.com")
-      end
+        super || guest_user
+    end
+
+    def guest_user
+      guest = GuestUser.new
+      guest.name = "Guest User"
+      guest.first_name = "Guest"
+      guest.last_name = "User"
+      guest.email = "guest@gmail.com"
+      guest
+    end
     
 end
